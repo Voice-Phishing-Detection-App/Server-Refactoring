3229069f00113905c91113b231fb211c5e06e12c
Yoo <hiyoojeong@gmail.com>
2023-05-10 03:56:20 +0900

    Feat: Center, Victim Entity 추가

---
+++ src/main/java/PhishingUniv/Phinocchio/Victim/entity/VictimEntity.java
@@ -0,0 +1,18 @@
+package PhishingUniv.Phinocchio.Victim.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "victim")
+public class VictimEntity {
+    @Id
+    @Column(name = "victime_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long victim_id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+}
