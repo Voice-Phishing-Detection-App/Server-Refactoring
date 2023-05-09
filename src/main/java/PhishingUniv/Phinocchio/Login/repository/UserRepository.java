220060d845820f164542d855152e9e7bf1f43503
Yoo <hiyoojeong@gmail.com>
2023-05-09 15:02:48 +0900

    Rename: User Entity 분리

--- src/main/java/PhishingUniv/Phinocchio/Login/repository/UserRepository.java
+++ src/main/java/PhishingUniv/Phinocchio/Login/repository/UserRepository.java
@@ -1,18 +1,18 @@
 package PhishingUniv.Phinocchio.Login.repository;
 
-import PhishingUniv.Phinocchio.Login.entity.User;
+import PhishingUniv.Phinocchio.User.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
 
-    Optional<User> findById(String id);
-    Optional<User> findByName(String name);
+    Optional<UserEntity> findById(String id);
+    Optional<UserEntity> findByName(String name);
 
-    List<User> findAll();
+    List<UserEntity> findAll();
 
 
 }
