96aa62bbc4b46efd3aad98f4921783f3a0df7509
Yoo <hiyoojeong@gmail.com>
2023-06-04 15:59:22 +0900

    Feat: 목소리 텍스트 데이터 저장 기능 추가

---
+++ src/main/java/PhishingUniv/Phinocchio/Voice/dto/VoiceDto.java
@@ -0,0 +1,14 @@
+package PhishingUniv.Phinocchio.Voice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VoiceDto {
+
+    private Long voiceId;
+
+    private String text;
+
+}
