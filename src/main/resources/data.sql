6ec01261635ea3bbb8016b5b23c87bcba938d358
Yoo <hiyoojeong@gmail.com>
2023-06-04 15:55:25 +0900

    Add: DB 초기 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO "user" (name, id, password, phone_number) VALUES ('조유정', 'admin', 'admin', '010-1234-5678');
+
+INSERT INTO setting (user_id, alram, detect_alram, sos_alram, sos_level) VALUES (1, 1, 1, 1, 1);
