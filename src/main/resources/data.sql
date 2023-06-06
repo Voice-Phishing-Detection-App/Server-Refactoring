efdaf92a216b9c8237985f29a1f79dea6a69ddaf
Yoo <hiyoojeong@gmail.com>
2023-06-06 23:32:07 +0900

    test: user 테스트 데이터 추가 (id: admin, password: admin)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO "user" (name, id, password, phone_number) VALUES ('조유정', 'admin', 'admin', '010-1234-5678');
+INSERT INTO "user" (name, id, password, phone_number) VALUES ('조유정', 'admin', '$2a$10$XLXsZbafQ/6hGxZ6NQ3ZvOXbN/GkCBc1oP3/wJ/gC6qAay9tfe8b2', '010-1234-5678');
 
 INSERT INTO setting (user_id, alram, detect_alram, sos_alram, sos_level) VALUES (1, 1, 1, 1, 1);
