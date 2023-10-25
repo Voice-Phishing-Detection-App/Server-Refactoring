ad8eca06966674f3d2b6237cd9b2565766cdf4f4
miiiinju1 <gms07073@ynu.ac.kr>
2023-10-26 01:08:34 +0900

    :sparkles: [FEAT] swagger 설정 #53
    close #53

--- src/main/java/PhishingUniv/Phinocchio/domain/Twilio/controller/TwilioTokenController.java
+++ src/main/java/PhishingUniv/Phinocchio/domain/Twilio/controller/TwilioTokenController.java
@@ -4,6 +4,8 @@ import PhishingUniv.Phinocchio.domain.Twilio.dto.TwilioTokenDto;
 import PhishingUniv.Phinocchio.domain.Twilio.dto.TwilioTokenRequestDto;
 import PhishingUniv.Phinocchio.domain.Twilio.service.TwilioTokenService;
 import com.twilio.jwt.accesstoken.AccessToken;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/twilio")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "TwilioTokenController", description = "Twilio Token을 생성하는 컨트롤러")
 public class TwilioTokenController {
 
     private final TwilioTokenService twilioTokenService;
 
     @PostMapping("/token")
+    @Operation(summary = "Twilio Token을 생성하는 컨트롤러", description = "Twilio Token을 생성하는 컨트롤러입니다.")
     public ResponseEntity<?> generateToken(@RequestBody TwilioTokenRequestDto twilioTokenRequestDto) {
         AccessToken token = twilioTokenService.generateToken(twilioTokenRequestDto);
 
