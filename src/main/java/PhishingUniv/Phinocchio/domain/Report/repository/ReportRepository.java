58de26b4d76eddc8efc204641629919f329b8454
ssuzyn <asj7090@gmail.com>
2024-01-15 19:10:34 +0900

    :art: fix: report 연관관계 적용에 따른 repository 코드 수정

--- src/main/java/PhishingUniv/Phinocchio/domain/Report/repository/ReportRepository.java
+++ src/main/java/PhishingUniv/Phinocchio/domain/Report/repository/ReportRepository.java
@@ -1,14 +1,15 @@
 package PhishingUniv.Phinocchio.domain.Report.repository;
 
 import PhishingUniv.Phinocchio.domain.Report.entity.ReportEntity;
+import PhishingUniv.Phinocchio.domain.User.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<ReportEntity, Long> {
-    Optional<ReportEntity> findByUserId(Long id);
-    List<ReportEntity> findReportEntitiesByUserId(Long userid);
+    Optional<ReportEntity> findByUser(UserEntity user);
+    List<ReportEntity> findReportEntitiesByUser(UserEntity user);
 
 
     List<ReportEntity> findReportEntitiesByPhoneNumber(String phoneNumber);
