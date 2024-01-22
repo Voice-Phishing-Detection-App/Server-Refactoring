fc266353e48b79ccf5f28b0c4ad708583706ab20
Yoo <hiyoojeong@gmail.com>
2024-01-22 21:39:19 +0900

    :goal_net: fix: Voice 관련 전역 예외처리 설정 누락 수정

--- src/main/java/PhishingUniv/Phinocchio/exception/ExceptionManager.java
+++ src/main/java/PhishingUniv/Phinocchio/exception/ExceptionManager.java
@@ -9,6 +9,7 @@ import PhishingUniv.Phinocchio.exception.Login.LoginAppException;
 import PhishingUniv.Phinocchio.exception.Setting.SettingAppException;
 import PhishingUniv.Phinocchio.exception.Sos.SosAppException;
 import PhishingUniv.Phinocchio.exception.Twilio.TwilioAppException;
+import PhishingUniv.Phinocchio.exception.Voice.VoiceAppException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,72 +22,70 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Slf4j
 public class ExceptionManager extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(DoubtAppException.class)
-    public ResponseEntity<?> doubtAppExceptionHandler(DoubtAppException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(DoubtAppException.class)
+  public ResponseEntity<?> doubtAppExceptionHandler(DoubtAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    @ExceptionHandler(STTAppException.class)
-    public ResponseEntity<?> sttAppExceptionHandler(STTAppException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(VoiceAppException.class)
+  public ResponseEntity<?> voiceAppExceptionHandler(VoiceAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    @ExceptionHandler(FCMAppException.class)
-    public ResponseEntity<?> fcmAppExceptionHandler(FCMAppException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(STTAppException.class)
+  public ResponseEntity<?> sttAppExceptionHandler(STTAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    @ExceptionHandler(InvalidJwtException.class)
-    public ResponseEntity<?> invalidJwtExceptionHandler(InvalidJwtException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(FCMAppException.class)
+  public ResponseEntity<?> fcmAppExceptionHandler(FCMAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    @ExceptionHandler(LoginAppException.class)
-    public ResponseEntity<?> loginAppExceptionHandler(LoginAppException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(InvalidJwtException.class)
+  public ResponseEntity<?> invalidJwtExceptionHandler(InvalidJwtException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    @ExceptionHandler(SettingAppException.class)
-    public ResponseEntity<?> settingAppExceptionHandler(SettingAppException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(LoginAppException.class)
+  public ResponseEntity<?> loginAppExceptionHandler(LoginAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    @ExceptionHandler(SosAppException.class)
-    public ResponseEntity<?> sosAppExceptionHandler(SosAppException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(SettingAppException.class)
+  public ResponseEntity<?> settingAppExceptionHandler(SettingAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    @ExceptionHandler(TwilioAppException.class)
-    public ResponseEntity<?> twilioAppExceptionHandler(TwilioAppException e)
-    {
-        ErrorCode errorCode = e.getErrorCode();
-        return handleExceptionInternal(errorCode);
-    }
+  @ExceptionHandler(SosAppException.class)
+  public ResponseEntity<?> sosAppExceptionHandler(SosAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    private ResponseEntity<?> handleExceptionInternal(ErrorCode errorCode) {
-        return ResponseEntity.status(errorCode.getHttpStatus())
-                .body(makeErrorResponse(errorCode));
-    }
+  @ExceptionHandler(TwilioAppException.class)
+  public ResponseEntity<?> twilioAppExceptionHandler(TwilioAppException e) {
+    ErrorCode errorCode = e.getErrorCode();
+    return handleExceptionInternal(errorCode);
+  }
 
-    private Object makeErrorResponse(ErrorCode errorCode) {
-        return ErrorResponse.builder()
-                .error(errorCode.name())
-                .message(errorCode.getMessage())
-                .build();
-    }
+  private ResponseEntity<?> handleExceptionInternal(ErrorCode errorCode) {
+    return ResponseEntity.status(errorCode.getHttpStatus())
+        .body(makeErrorResponse(errorCode));
+  }
+
+  private Object makeErrorResponse(ErrorCode errorCode) {
+    return ErrorResponse.builder()
+        .error(errorCode.name())
+        .message(errorCode.getMessage())
+        .build();
+  }
 
 }
