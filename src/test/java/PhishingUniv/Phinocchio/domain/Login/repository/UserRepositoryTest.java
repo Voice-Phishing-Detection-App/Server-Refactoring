af69c2818ba8a0c598c30f75724a6ecee49bff1f
ssuzyn <asj7090@gmail.com>
2024-01-25 13:40:55 +0900

    :white_check_mark: test: 아이디로 사용자 엔티티 조회

---
+++ src/test/java/PhishingUniv/Phinocchio/domain/Login/repository/UserRepositoryTest.java
@@ -0,0 +1,51 @@
+package PhishingUniv.Phinocchio.domain.Login.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import PhishingUniv.Phinocchio.domain.Login.dto.SignupRequestDto;
+import PhishingUniv.Phinocchio.domain.User.entity.UserEntity;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserRepositoryTest {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Test
+  @DisplayName("아이디로 사용자 엔티티 조회")
+  public void testFindById() {
+    // given
+    UserEntity user = new UserEntity(signupRequestDto());
+    userRepository.save(user);
+
+    // when
+    Optional<UserEntity> foundUser = userRepository.findById("userId");
+
+    // then
+    assertThat(foundUser.isPresent());
+    assertEquals("userId", foundUser.get().getId());
+  }
+
+
+  SignupRequestDto signupRequestDto(){
+
+    SignupRequestDto signupRequestDto = new SignupRequestDto();
+    signupRequestDto.setId("userId");
+    signupRequestDto.setName("userName");
+    signupRequestDto.setPassword("password");
+    signupRequestDto.setPhoneNumber("01012340000");
+    signupRequestDto.setFcmToken("fcmToken");
+
+    return signupRequestDto;
+  }
+
+}
