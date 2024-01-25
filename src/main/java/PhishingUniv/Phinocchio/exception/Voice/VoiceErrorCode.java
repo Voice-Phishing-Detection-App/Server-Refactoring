25e13034066aa3cce90d587355e734a815e8030b
ssuzyn <asj7090@gmail.com>
2024-01-26 00:06:58 +0900

    :sparkles: add: 존재하지 않는 voice 조회하는 경우 에러코드 설정

--- src/main/java/PhishingUniv/Phinocchio/exception/Voice/VoiceErrorCode.java
+++ src/main/java/PhishingUniv/Phinocchio/exception/Voice/VoiceErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum VoiceErrorCode implements ErrorCode {
 
-    FAILED_TO_SAVE(HttpStatus.INTERNAL_SERVER_ERROR, "목소리 저장에 실패하였습니다.");
+    FAILED_TO_SAVE(HttpStatus.INTERNAL_SERVER_ERROR, "목소리 저장에 실패하였습니다."),
+    VOICE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 목소리 정보 입니다.");
 
     private HttpStatus httpStatus;
     private String message;
