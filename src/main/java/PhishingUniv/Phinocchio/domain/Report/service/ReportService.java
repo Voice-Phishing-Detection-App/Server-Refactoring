dc469d2163cd4b467368a24e6e907b75a924c300
ssuzyn <asj7090@gmail.com>
2024-01-26 01:46:01 +0900

    :recycle: refactor: 메서드 분리

--- src/main/java/PhishingUniv/Phinocchio/domain/Report/service/ReportService.java
+++ src/main/java/PhishingUniv/Phinocchio/domain/Report/service/ReportService.java
@@ -2,83 +2,83 @@ package PhishingUniv.Phinocchio.domain.Report.service;
 
 
 import PhishingUniv.Phinocchio.domain.Doubt.entity.DoubtEntity;
-import PhishingUniv.Phinocchio.domain.Doubt.repository.DoubtRepository;
-import PhishingUniv.Phinocchio.domain.Login.repository.UserRepository;
+import PhishingUniv.Phinocchio.domain.Doubt.service.DoubtService;
+import PhishingUniv.Phinocchio.domain.Login.service.UserService;
 import PhishingUniv.Phinocchio.domain.Report.dto.ReportDto;
 import PhishingUniv.Phinocchio.domain.Report.dto.ReportWithoutDoubtDto;
 import PhishingUniv.Phinocchio.domain.Report.entity.ReportEntity;
 import PhishingUniv.Phinocchio.domain.Report.repository.ReportRepository;
 import PhishingUniv.Phinocchio.domain.User.entity.UserEntity;
 import PhishingUniv.Phinocchio.domain.Voice.entity.VoiceEntity;
-import PhishingUniv.Phinocchio.domain.Voice.repository.VoiceRepository;
+import PhishingUniv.Phinocchio.domain.Voice.service.VoiceService;
 import PhishingUniv.Phinocchio.exception.Doubt.DoubtAppException;
-import PhishingUniv.Phinocchio.exception.Doubt.DoubtErrorCode;
 import PhishingUniv.Phinocchio.exception.Login.InvalidJwtException;
-import PhishingUniv.Phinocchio.exception.Login.LoginErrorCode;
+import PhishingUniv.Phinocchio.exception.Voice.VoiceAppException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class ReportService {
-    private final DoubtRepository doubtRepository;
+    private final DoubtService doubtService;
     private final ReportRepository reportRepository;
-    private final VoiceRepository voiceRepository;
+    private final VoiceService voiceService;
+    private final UserService userService;
 
-    //userId 가져오기 위해서 임시로 만들어둔 것, 캐싱으로 나중에 수정해야하는 부분
-    private final UserRepository userRepository;
-    public ReportEntity addReport(ReportDto reportDto) {
-        String ID = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity userEntity =userRepository.findById(ID).orElseThrow(
-                ()->new InvalidJwtException(LoginErrorCode.JWT_USER_NOT_FOUND));
-
-        Optional<VoiceEntity> optionalVoiceEntity = voiceRepository.findByVoiceId(reportDto.getVoiceId());
-        VoiceEntity voice = optionalVoiceEntity.orElseThrow(() -> new RuntimeException("해당 voice table이 존재하지 않습니다."));
-
-        ReportEntity reportEntity = new ReportEntity(reportDto.getType(), reportDto.getTitle(), reportDto.getContent()
-                                                ,reportDto.getPhoneNumber(),userEntity, voice);
+    public ReportEntity addReport(ReportDto reportDto) throws InvalidJwtException, DoubtAppException, VoiceAppException {
+        UserEntity userEntity = ensureUserAuthenticated();
+        VoiceEntity voiceEntity = voiceService.findByVoiceId(reportDto.getVoiceId());
+        ReportEntity reportEntity = createReportEntity(reportDto, userEntity, voiceEntity);
 
         reportRepository.save(reportEntity);
-
-        DoubtEntity doubtEntity = doubtRepository.findById(reportDto.getDoubtId()).orElseThrow(() -> new DoubtAppException(
-                DoubtErrorCode.DOUBT_NOT_FOUND));
-        doubtEntity.setReport(reportEntity);
-        doubtRepository.save(doubtEntity);
+        updateDoubtEntity(reportDto, reportEntity);
 
         return reportEntity;
     }
 
-    public ReportEntity addReportWithoutDoubt(ReportWithoutDoubtDto reportDto)throws InvalidJwtException
-    {
-        String ID = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity userEntity =userRepository.findById(ID).orElseThrow(
-                ()->new InvalidJwtException(LoginErrorCode.JWT_USER_NOT_FOUND));
-
-        ReportEntity reportEntity = ReportEntity.builder()
-                .type(reportDto.getType())
-                .title(reportDto.getTitle())
-                .content(reportDto.getContent())
-                .phoneNumber(reportDto.getPhoneNumber())
-                .user(userEntity).build();
+    public ReportEntity addReportWithoutDoubt(ReportWithoutDoubtDto reportDto)throws InvalidJwtException {
+        UserEntity userEntity = ensureUserAuthenticated();
+        ReportEntity reportEntity = createReportWithoutDoubtEntity(reportDto, userEntity);
 
         return reportRepository.save(reportEntity);
     }
 
-    public List<ReportEntity> getReports() throws InvalidJwtException
-    {
-        String ID = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity userEntity =userRepository.findById(ID).orElseThrow(
-                ()->new InvalidJwtException(LoginErrorCode.JWT_USER_NOT_FOUND));
+    public List<ReportEntity> getReports() throws InvalidJwtException{
+        UserEntity userEntity = ensureUserAuthenticated();
 
-        List<ReportEntity> reportEntities = reportRepository.findReportEntitiesByUser(userEntity);
-
-        return reportEntities;
+      return reportRepository.findReportEntitiesByUser(userEntity);
+    }
 
 
+    private UserEntity ensureUserAuthenticated() throws InvalidJwtException {
+        return userService.getCurrentUser();
+    }
+
+    private void updateDoubtEntity(ReportDto reportDto, ReportEntity reportEntity){
+        DoubtEntity doubtEntity = doubtService.findByDoubtId(reportDto.getDoubtId());
+        doubtEntity.setReport(reportEntity);
+        doubtService.save(doubtEntity);
+    }
+
+    private ReportEntity createReportEntity(ReportDto reportDto, UserEntity userEntity, VoiceEntity voiceEntity) {
+        return ReportEntity.builder()
+            .type(reportDto.getType())
+            .title(reportDto.getTitle())
+            .content(reportDto.getContent())
+            .phoneNumber(reportDto.getPhoneNumber())
+            .user(userEntity)
+            .voice(voiceEntity).build();
+    }
+
+    private ReportEntity createReportWithoutDoubtEntity(ReportWithoutDoubtDto reportDto, UserEntity userEntity){
+        return ReportEntity.builder()
+            .type(reportDto.getType())
+            .title(reportDto.getTitle())
+            .content(reportDto.getContent())
+            .phoneNumber(reportDto.getPhoneNumber())
+            .user(userEntity).build();
     }
 
 }
